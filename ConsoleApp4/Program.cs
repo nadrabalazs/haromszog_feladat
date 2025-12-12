fa2610e29c289e742a8bc39b630967376bca48e9
nadra.balazs <nadra.balazs@57-11>
2025-12-12 13:24:35 +0100

    merge conflict

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -5,25 +5,22 @@ namespace ConsoleApp18
     {
         static void Main(string[] args)
         {
-            ValasztasMenubol();
-            TeruletSzamitas();
+            UdvozloKep();
             List<string> menupontok = new List<string>();
             ListazMenu(menupontok);
             ValasztasMenubol(menupontok);
             menupontok.Add("Kerület számítása");
             menupontok.Add("Terület számítása");
             menupontok.Add("Kilépés");
-            Pitagorasz();
-
             Console.WriteLine("Vége!");
         }
-
-        
 
         private static void UdvozloKep()
         {
             Console.Clear();
             Console.SetCursorPosition(30, 0);
+            Console.WriteLine("Üdvözlöm a háromszög szuper alkalmazásban!");
+        }
         private static void ListazMenu(List<string> menupontok)
         {
             Console.SetCursorPosition(35, 1);
@@ -31,21 +28,12 @@ namespace ConsoleApp18
             Console.SetCursorPosition(35, 2);
             Console.WriteLine("Terület, Nyomd meg az 2-est!");
             Console.SetCursorPosition(35, 3);
-            Console.WriteLine("Magasságtétel, Nyomd meg az 3-ast!");
+            Console.WriteLine("Pitagorasz, Nyomd meg az 3-ast!");
         }
 
 
         private static void ValasztasMenubol(List<string> menupontok)
-        
-        
-        private static void Pitagorasz()
-
-            Console.WriteLine("Add meg az a négyzetét és b Négyzetét: ");
-            double aNegyzet = Convert.ToDouble(Console.ReadLine());
-            double bNegyzet = Convert.ToDouble(Console.ReadLine());
-            double cNegyzet = Convert.ToDouble(Math.Sqrt(Math.Pow(aNegyzet,2))+Math.Pow(bNegyzet,2));
-            Console.WriteLine($"C oldal = {cNegyzet}");
-            throw new NotImplementedException();
+        {
             switch (Convert.ToInt32(Console.ReadLine()))
             {
                 case 1:
@@ -54,7 +42,9 @@ namespace ConsoleApp18
                 case 2:
                     TeruletSzamitas();
                     break;
-                
+                case 3:
+                    Pitagorasz();
+                    break;
 
             }
         }
@@ -75,10 +65,15 @@ namespace ConsoleApp18
             double a = Convert.ToDouble(Console.ReadLine());
             double ma = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine($"Terület: {(a * ma) / 2}");
-            Console.WriteLine((a*ma)/2);
-            Console.WriteLine((a*ma)/2);
-            double ma = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine((a*ma)/2);
         }
+        private static void Pitagorasz() 
+        { 
+            Console.WriteLine("Add meg az a négyzetét és b Négyzetét: ");
+            double aNegyzet = Convert.ToDouble(Console.ReadLine());
+            double bNegyzet = Convert.ToDouble(Console.ReadLine());
+            double cNegyzet = Convert.ToDouble(Math.Sqrt(Math.Pow(aNegyzet, 2)) + Math.Pow(bNegyzet, 2));
+            Console.WriteLine($"C oldal = {cNegyzet}");
+            throw new NotImplementedException();
     }
+}
 }
