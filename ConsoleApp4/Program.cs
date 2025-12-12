0aaa863cb425ce8ea57db1399aff35c3df74ab13
greguss.illes <greguss.illes@57-12>
2025-12-12 13:25:09 +0100

    utolso v

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -8,6 +8,17 @@ namespace ConsoleApp18
             UdvozloKep();
             Console.WriteLine("Vége!");
             Magassagtetel();
+            koreirtkor();
+        }
+
+        private static void koreirtkor()
+        {
+            Console.WriteLine("Adja meg a háromszög oldalait és a területét:  ");
+            double a =Convert.ToDouble (Console.ReadLine());
+            double b =Convert.ToDouble (Console.ReadLine());
+            double c =Convert.ToDouble (Console.ReadLine());
+            double Ter =Convert.ToDouble (Console.ReadLine());
+            Console.WriteLine($"A háromszög köré írt kör sugara: {(a+b+c)/4*Ter}");
         }
 
         private static void Magassagtetel()
@@ -15,7 +26,7 @@ namespace ConsoleApp18
             Console.WriteLine("Adja meg a háromszög területét és az egyik oldalát:");
             double oldal = Convert.ToDouble(Console.ReadLine());
             double T = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine((T * 2) / oldal);
+            Console.WriteLine($"Az oldalhoz tartozó magasság:{(T * 2) / oldal} ");
 
         }
 
