dce34c85d2f7e7f50bca904e7d088ba37d1fe809
Toshiba <Toshiba@DESKTOP-10NVPD0>
2025-12-14 11:16:36 +0100

    visszalepes a fomenube

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,20 +1,11 @@
 ﻿
-namespace ConsoleApp18
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             UdvozloKep();
-            List<string> menupontok = new List<string>();
-            menupontok.Add("Kerület számítása(F1)");
-            menupontok.Add("Terület számítása(F2)");
-            menupontok.Add("Pitagorasz tétel(F3)");
-            menupontok.Add("Magasság tétel(F4)");
-            menupontok.Add("Köré írható kör sugara(F5)");
-            menupontok.Add("Kilépés(Esc)");
-            ListazMenu(menupontok);
-            ValasztasMenubol(menupontok);
             Console.WriteLine("Vége!");
         }
         private static void ListazMenu(List<string> menupontok)
@@ -39,11 +30,20 @@ namespace ConsoleApp18
             Console.Clear();
             Console.SetCursorPosition(30, 0);
             Console.WriteLine("Üdvözlöm a háromszög szuper alkalmazásban!\n");
+            List<string> menupontok = new List<string>();
+            menupontok.Add("Kerület számítása(F1)");
+            menupontok.Add("Terület számítása(F2)");
+            menupontok.Add("Pitagorasz tétel(F3)");
+            menupontok.Add("Magasság tétel(F4)");
+            menupontok.Add("Köré írható kör sugara(F5)");
+            menupontok.Add("Kilépés(Esc)");
+            ListazMenu(menupontok);
+            ValasztasMenubol(menupontok);
         }
         private static void Kilepes()
         {
             Console.Clear();
-            Console.SetCursorPosition(40,5);
+            Console.SetCursorPosition(40, 5);
             Console.WriteLine("Viszont látásra!");
             // TODO: Nádra Kilepes
         }
@@ -64,43 +64,43 @@ namespace ConsoleApp18
                 case ConsoleKey.F4:
                     MagassagTetel();
                     break;
-                case ConsoleKey.F5: 
-                    KoreIrtKor(); 
+                case ConsoleKey.F5:
+                    KoreIrtKor();
                     break;
                 case ConsoleKey.Escape:
                     Kilepes();
                     break;
-                // TODO: Nádra ValasztasMenubol
+                    // TODO: Nádra ValasztasMenubol
             }
         }
         private static void KoreIrtKor()
         {
             Console.Clear();
-            Console.WriteLine("Kilépéshez nyomja meg az Escape gombot!\nAdja meg a háromszög oldalait és a területét:");
+            Console.WriteLine("Kilépés a főmenübe(Esc)!\nAdja meg a háromszög oldalait és a területét:");
             ConsoleKeyInfo keyInfo = Console.ReadKey(true);
             if (keyInfo.Key == ConsoleKey.Escape)
             {
-                Kilepes();
+                UdvozloKep();
                 return;
             }
-            double a =Convert.ToDouble (Console.ReadLine());
-            double b =Convert.ToDouble (Console.ReadLine());
-            double c =Convert.ToDouble (Console.ReadLine());
-            double Ter =Convert.ToDouble (Console.ReadLine());
-            Console.WriteLine($"A háromszög köré írható kör sugara: {(a+b+c)/4*Ter}");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            double Ter = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"A háromszög köré írható kör sugara: {(a + b + c) / 4 * Ter}");
             // TODO: Illés KoreIrtKor
         }
         private static void MagassagTetel()
         {
             Console.Clear();
-            Console.WriteLine("Kilépéshez nyomja meg az Escape gombot!\nAdja meg a háromszög területét és az egyik oldalát:");
+            Console.WriteLine("Kilépés a főmenübe(Esc)!\nAdja meg a háromszög területét és az egyik oldalát:");
+            Console.WriteLine("Terület:");
             ConsoleKeyInfo keyInfo = Console.ReadKey(true);
             if (keyInfo.Key == ConsoleKey.Escape)
             {
-                Kilepes();
+                UdvozloKep();
                 return;
             }
-            Console.WriteLine("Terület:");
             double T = Convert.ToDouble(Console.ReadLine());
             double oldal = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine($"Az oldalhoz tartozó magasság:{(T * 2) / oldal} ");
@@ -110,11 +110,11 @@ namespace ConsoleApp18
         private static void KeruletSzamitas()
         {
             Console.Clear();
-            Console.WriteLine("Kilépéshez nyomja meg az Escape gombot!\nAdd meg a három oldal hosszát:");
+            Console.WriteLine("Kilépés a főmenübe(Esc)!\nAdd meg a három oldal hosszát:");
             ConsoleKeyInfo keyInfo = Console.ReadKey(true);
             if (keyInfo.Key == ConsoleKey.Escape)
             {
-                Kilepes();
+                UdvozloKep();
                 return;
             }
             double a = Convert.ToDouble(Console.ReadLine());
@@ -126,11 +126,11 @@ namespace ConsoleApp18
         private static void TeruletSzamitas()
         {
             Console.Clear();
-            Console.WriteLine("Kilépéshez nyomja meg az Escape gombot!\nAdd meg egy alap hosszát és a háromszög magasságát: ");
+            Console.WriteLine("Kilépés a főmenübe(Esc)!\nAdd meg egy alap hosszát és a háromszög magasságát: ");
             ConsoleKeyInfo keyInfo = Console.ReadKey(true);
             if (keyInfo.Key == ConsoleKey.Escape)
             {
-                Kilepes();
+                UdvozloKep();
                 return;
             }
             double a = Convert.ToDouble(Console.ReadLine());
@@ -138,20 +138,21 @@ namespace ConsoleApp18
             Console.WriteLine($"Terület: {(a * ma) / 2}");
             // TODO: Nádra TeruletSzamitas
         }
-        private static void Pitagorasz() 
+        private static void Pitagorasz()
         {
             Console.Clear();
-            Console.WriteLine("Kilépéshez nyomja meg az Escape gombot!\nAdd meg az a és b oldal hosszát:");
+            Console.WriteLine("Kilépés a főmenübe(Esc)!\nAdd meg az a és b oldal hosszát:");
             ConsoleKeyInfo keyInfo = Console.ReadKey(true);
             if (keyInfo.Key == ConsoleKey.Escape)
             {
-                Kilepes();
+                UdvozloKep();
                 return;
             }
             double aNegyzet = Convert.ToDouble(Console.ReadLine());
             double bNegyzet = Convert.ToDouble(Console.ReadLine());
             double cNegyzet = Convert.ToDouble(Math.Sqrt((Math.Pow(aNegyzet, 2)) + Math.Pow(bNegyzet, 2)));
-            Console.WriteLine($"C oldal = {cNegyzet}");    }
+            Console.WriteLine($"C oldal = {cNegyzet}");
+        }
         // TODO: Simon Pitagorasz
     }
 }
